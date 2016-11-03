4defd6e33571f26974b76e0d49e1e0517b9094fb
Royalgenius <icy333_sun@sina.com>
2016-11-03 19:18:55 +0800

    Add files via upload
    print ('I\'m ok.')
    print ('I\'m learning\nPython.')
    print ('\\\n\\')
    print ('\\\t\\')
    print (r'\\\t\\')
    print ('''line1
    ...line2
    ...line3''')
    age =int(input('please enter your age:'))
    if age >=18:
    	print('adult')
    else:
    	print('teenager')
    a='ABC'
    b=a
    a='XYZ'
    print(b)

---
+++ 04_datatype.py
@@ -0,0 +1,17 @@
+print ('I\'m ok.')
+print ('I\'m learning\nPython.')
+print ('\\\n\\')
+print ('\\\t\\')
+print (r'\\\t\\')
+print ('''line1
+...line2
+...line3''')
+age =int(input('please enter your age:'))
+if age >=18:
+	print('adult')
+else:
+	print('teenager')
+a='ABC'
+b=a
+a='XYZ'
+print(b)
