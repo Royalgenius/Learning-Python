ca52cf4fbb5b9f4e057f95ce207574c2422a907b
Royalgenius <icy333_sun@sina.com>
2016-11-03 20:09:42 +0800

    Add files via upload
    n=123
    f=456.789
    s1='Hello,world'
    s11='\'Hello,world\''
    s2='\'Hello, \\\'Adam\\\'\''
    s21 = 'Hello,\'Adam\''
    s3=r'Hello,"Bart"'
    s4=r'''Hello,
    Lisa!'''
    print ('n=',n)
    print ('f=',f)
    print ('s1=',s1)
    print ('s11=',s11)
    print ('s2=',s2)
    print ('s21=',s21)
    print ('s3=',s3)
    print ('s4=',s4)

---
+++ 04_datatype_exercise.py
@@ -0,0 +1,18 @@
+
+n=123
+f=456.789
+s1='Hello,world'
+s11='\'Hello,world\''
+s2='\'Hello, \\\'Adam\\\'\''
+s21 = 'Hello,\'Adam\''
+s3=r'Hello,"Bart"'
+s4=r'''Hello,
+Lisa!'''
+print ('n=',n)
+print ('f=',f)
+print ('s1=',s1)
+print ('s11=',s11)
+print ('s2=',s2)
+print ('s21=',s21)
+print ('s3=',s3)
+print ('s4=',s4)
